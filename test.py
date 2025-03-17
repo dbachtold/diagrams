243856a59c2898ff9b8e1f1583bd412cbdb080db
dbachtold <dbachtold@kio.co.uk>
2025-03-17 10:26:00 +0000

    first commit from codespace

---
+++ test.py
@@ -0,0 +1,6 @@
+#code space location: https://miniature-journey-wr5gpxvj7vp63967x.github.dev/
+#github get out of jail free: git reset HEAD~1
+
+import random
+rand_int   = random.randint(1,10)
+print(f"hello world {rand_int}")
